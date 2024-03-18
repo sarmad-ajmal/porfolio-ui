f8fbcaef953d56faf7103bf482aa760e372c6d8c
Sarmad Ajmal <24247576+sarmad-ajmal@users.noreply.github.com>
2024-03-18 16:15:52 +0400

    update target blank in urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import UnderConstruction from "@/components/under_development";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,11 +9,14 @@ export default function Home() {
     <main className="flex max-h-screen flex-col items-center justify-between p-12">
       <UnderConstruction />
       <div className="font-medium text-2xl font-mono text-cyan-700">
-        
-      <TypeIt>Something amazing is on its way...</TypeIt>
+        <TypeIt>Something amazing is on its way...</TypeIt>
       </div>
       <div className="flex justify-center items-center mt-3">
-        <Link href={"https://github.com/sarmad-ajmal"} target="" className="pr-5">
+        <Link
+          href={"https://github.com/sarmad-ajmal"}
+          target="_blank"
+          className="pr-5"
+        >
           <Image
             src="/assets/images/github.svg"
             alt="github"
@@ -21,7 +24,11 @@ export default function Home() {
             height={50}
           />
         </Link>
-        <Link href={"https://www.linkedin.com/in/sarmad-ajmal/"} className="pr-5">
+        <Link
+          href={"https://www.linkedin.com/in/sarmad-ajmal/"}
+          target="_blank"
+          className="pr-5"
+        >
           <Image
             src="/assets/images/linkedin.svg"
             alt="linkedin"
@@ -29,7 +36,11 @@ export default function Home() {
             height={50}
           />
         </Link>
-        <Link href={"https://twitter.com/SarmadAjmal"} className="pr-5">
+        <Link
+          href={"https://twitter.com/SarmadAjmal"}
+          target="_blank"
+          className="pr-5"
+        >
           <Image
             src="/assets/images/twitter.svg"
             alt="twitter"
