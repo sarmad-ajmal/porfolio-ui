6361d61c35435729554a657e19f61d11d10fbf30
Sarmad Ajmal <24247576+sarmad-ajmal@users.noreply.github.com>
2024-03-19 14:46:41 +0400

    add google analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
+``;
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,8 +16,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const G_TAG = process.env.NEXT_PUBLIC_G_TAG;
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${G_TAG}`}
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${G_TAG}');
+        `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
